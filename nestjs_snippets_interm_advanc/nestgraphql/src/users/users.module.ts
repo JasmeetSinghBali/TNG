863a8cb6d0ad7087504cc183b741ea382f6642fb
Jasmeet-1998 <jasmeetbali.dev.2021@gmail.com>
2022-07-02 11:47:38 +0530

    register userServices & userResolver user.module

--- nestjs_snippets_interm_advanc/nestgraphql/src/users/users.module.ts
+++ nestjs_snippets_interm_advanc/nestgraphql/src/users/users.module.ts
@@ -1,4 +1,9 @@
 import { Module } from "@nestjs/common";
+import { UsersResolver } from "./users.resolver";
+import { UsersService } from "./users.service";
 
-@Module({})
+@Module({
+    // register the userResolver & userService in user entity user.module.ts
+    providers: [UsersResolver,UsersService]
+})
 export class UsersModule {}
--- nestjs_snippets_interm_advanc/nestgraphql/src/users/users.resolver.ts
+++ nestjs_snippets_interm_advanc/nestgraphql/src/users/users.resolver.ts
@@ -16,29 +16,29 @@ export class UsersResolver {
     // 🎈 in case of a real DB this function will be async and it will return promise rather than just the usermodelInterface
     @Query(()=>usermodelInterface,{name: 'user', nullable: true}) // tells nestjs what the return type is on the query via the anonymous function, we can provide the name of the query i.e route by default it will be the name of the method in this case getUser, nullable true means it will return null if we dont find a user
     getUser(@Args() getUserArgs: GetUserArgs): usermodelInterface{ // @Args() is a parameter decorator
-        return this.usersService.getUser();
+        return this.usersService.getUser(getUserArgs);
     }
 
     // nullable true means that we can expect the items in the list to be null but not the list itself, if you want to allow both of them can be nullable then itemsAndList
     @Query(()=>[usermodelInterface],{name: 'users', nullable: 'items' })
     getUsers(@Args() getUsersArgs: GetUsersArgs): usermodelInterface[]{
-        return this.usersService.getUsers();
+        return this.usersService.getUsers(getUsersArgs);
     }
 
     // 🎯 Setup- Mutations (Update,Create,Delete)
     // anonymous function inside mutation decorator that defines the return type of this mutation
     @Mutation(()=>usermodelInterface)
     createUser(@Args('createUserData') createUserData: CreateUserInput ): usermodelInterface{
-        return this.usersService.createUser();
+        return this.usersService.createUser(createUserData);
     }
 
     @Mutation(()=>usermodelInterface)
     updateUser(@Args('updateUserData') updateUserData: UpdateUserInput ): usermodelInterface{
-        return this.usersService.updateUser();
+        return this.usersService.updateUser(updateUserData);
     }
 
     @Mutation(()=>usermodelInterface)
     deleteUser(@Args('deleteUserData') deleteUserData: DeleteUserInput): usermodelInterface{
-        return this.usersService.deleteUser();
+        return this.usersService.deleteUser(deleteUserData);
     }
 }
