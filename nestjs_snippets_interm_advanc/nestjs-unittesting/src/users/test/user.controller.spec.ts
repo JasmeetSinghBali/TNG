290714aaf47aa12ca9947335b07a31a83a93ce0b
Jasmeet-1998 <jasmeetbali.dev.2021@gmail.com>
2022-07-05 19:30:15 +0530

    register module ref with original serv|contr
    beforeEach
    - ✔ sorted

--- nestjs_snippets_interm_advanc/nestjs-unittesting/src/users/test/user.controller.spec.ts
+++ nestjs_snippets_interm_advanc/nestjs-unittesting/src/users/test/user.controller.spec.ts
@@ -6,6 +6,10 @@ import { UsersService } from "../users.service"
 jest.mock('../../users.service');
 
 describe('UsersController',()=>{
+    //📝 reff for user controller
+    let usersController : UsersController;
+    let usersService: UsersService;
+
     beforeEach(async()=>{
         //📝 grab the user controller via creating a test module refference for testing it with jest
         const moduleRef = await Test.createTestingModule({
@@ -14,5 +18,12 @@ describe('UsersController',()=>{
             // depend required by UserController
             providers:[UsersService]
         }).compile();
-    })
+        
+        //📝 register the original controller and service reff to the testing module ref
+        usersController = moduleRef.get<UsersController>(UsersController);
+        usersService = moduleRef.get<UsersService>(UsersService);
+        
+        // always better to do this as this prevent uncecessary debugging, this will clear the mocks for each calls
+        jest.clearAllMocks();
+    });
 })
