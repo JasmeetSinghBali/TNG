aba1f32f551fae6f7d5d803b0487bc5a4d283169
Jasmeet-1998 <jasmeetbali.dev.2021@gmail.com>
2022-07-05 19:20:24 +0530

    completed mock stubs for user.service.ts

--- nestjs_snippets_interm_advanc/nestjs-unittesting/src/users/__mocks__/users.service.ts
+++ nestjs_snippets_interm_advanc/nestjs-unittesting/src/users/__mocks__/users.service.ts
@@ -1,5 +1,11 @@
+import { userStub } from "../test/stubs/user.stub";
+
 // 📝 mock UsersService just like the user.service.ts in the root of users so that jest can pick it up
 export const UsersService = jest.fn().mockReturnValue({
-    // returns the method that we want to mock
-    getUserById: jest.fn().mockReturnValue()
+    // returns the method that we want to mock along with the mock returned data
+    // 📝 mock resolved value simulates returns  promises responses just like it is in real user.service.ts
+    getUserById: jest.fn().mockResolvedValue(userStub()),
+    getUsers: jest.fn().mockResolvedValue([userStub()]),
+    createUser: jest.fn().mockResolvedValue(userStub()),
+    updateUser: jest.fn().mockResolvedValue(userStub()),
 })
