814b8d094c9d4295d491594e0f6bc97015c86996
Jasmeet-1998 <jasmeetbali.dev.2021@gmail.com>
2022-07-06 19:36:31 +0530

    tests mock with stubs given-when then
    covers
    - getUsers
    - updateUser
    - createUser

--- nestjs_snippets_interm_advanc/nestjs-unittesting/src/users/test/users.controller.spec.ts
+++ nestjs_snippets_interm_advanc/nestjs-unittesting/src/users/test/users.controller.spec.ts
@@ -1,4 +1,6 @@
 import { Test } from "@nestjs/testing"
+import { CreateUserDto } from "../dto/create-user.dto";
+import { UpdateUserDto } from "../dto/update-user.dto";
 import { User } from "../schemas/user.schema";
 import { UsersController } from "../users.controller"
 import { UsersService } from "../users.service"
@@ -44,6 +46,72 @@ describe('UsersController',()=>{
             })
 
             test('[then] it should also return a user',()=>{
+                expect(user).toEqual(userStub());
+            })
+        })
+    })
+    // 📝 unit-test 2 for getUsers(all) method in user.controller.ts with given-when-then fashion
+    describe('[given]get all users',()=>{
+        // 🎯when-block
+        describe('[when] getUsers is called',()=>{
+            let users: User[];
+            beforeEach(async () => {
+                users = await usersController.getUsers();
+            })
+            //🎯then-block (assertions actual expectation from the test)
+            test('[then] it should call user service',()=>{
+                expect(usersService.getUsers).toHaveBeenCalled();
+            })
+
+            test('[then] it should also return list/array of users',()=>{
+                expect(users).toEqual([userStub()]);
+            })
+        })
+    })
+    // 📝 unit-test 3 for createUser  method in user.controller.ts with given-when-then fashion
+    describe('[given]create new user',()=>{
+        // 🎯when-block
+        describe('[when] createUsers is called',()=>{
+            let user: User;
+            let createUserDto: CreateUserDto;
+            beforeEach(async () => {
+                createUserDto = {
+                    email: userStub().email,
+                    age: userStub().age
+                }
+                user = await usersController.createUser(createUserDto);
+            })
+            //🎯then-block (assertions actual expectation from the test)
+            test('[then] it should call user service user create',()=>{
+                expect(usersService.createUser).toBeCalledWith(createUserDto.email,createUserDto.age);
+            })
+
+            test('[then] it should also return the newly created user',()=>{
+                // 📝: IMP compare the original user created with controller and one with mock stub
+                expect(user).toEqual(userStub());
+            })
+        })
+    })
+    // 📝 unit-test 4 for updatedUser  method in user.controller.ts with given-when-then fashion
+    describe('[given]update new user',()=>{
+        // 🎯when-block
+        describe('[when] updateUser is called',()=>{
+            let user: User;
+            let updateUserDto: UpdateUserDto;
+            beforeEach(async () => {
+                updateUserDto = {
+                    age: 23,
+                    favoriteFoods: ['samosa']
+                }
+                user = await usersController.updateUser(userStub().userId,updateUserDto);
+            })
+            //🎯then-block (assertions actual expectation from the test)
+            test('[then] it should call user service userUpdate',()=>{
+                expect(usersService.updateUser).toHaveBeenCalledWith(userStub().userId,updateUserDto);
+            })
+
+            test('[then] it should also return the updated  user',()=>{
+                // 📝: IMP compare the original user created with controller and one with mock stub
                 expect(user).toEqual(userStub());
             })
         })
