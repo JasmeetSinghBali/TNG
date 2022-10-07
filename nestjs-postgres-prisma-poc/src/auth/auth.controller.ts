03f021a0f0de0204393066c1925927ae77c89f66
Jasmeet-1998 <devs.us.1984@gmail.com>
2022-10-07 12:46:04 +0530

    stp1: auth module,controller,service barebones
    next:
    - setup db postgresql as docker instance

---
+++ nestjs-postgres-prisma-poc/src/auth/auth.controller.ts
@@ -0,0 +1,20 @@
+import { Controller, Injectable, Post } from "@nestjs/common";
+import { AuthService } from "./auth.service";
+
+@Controller('auth')
+export class AuthController{
+    constructor(private readonly authService: AuthService) {
+    }
+
+    // /auth/signup
+    @Post('signup')
+    signup(){
+        return this.authService.signup();
+    }
+    
+    // /auth/signin
+    @Post('signin')
+    signin(){
+        return this.authService.signin();
+    }
+}
